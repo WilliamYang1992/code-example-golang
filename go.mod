b7dd0d72855aaeda066c5bd0b483b3bc0441e0e1
WilliamYang1992 <505741310@qq.com>
2019-12-21 18:10:06 +0800

    add redis

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module code-example-golang
 
 go 1.13
+
+require github.com/go-redis/redis/v7 v7.0.0-beta.4
---
+++ redis/redis_test.go
@@ -0,0 +1,42 @@
+package redis
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis/v7"
+)
+
+const (
+	RedisHost     = "localhost"
+	RedisPort     = 6379
+	RedisPassword = ""
+	DB            = 0
+)
+
+func Example() {
+	// 创建 client
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", RedisHost, RedisPort),
+		Password: RedisPassword,
+		DB:       DB,
+	})
+	// 连接测试
+	if _, err := client.Ping().Result(); err != nil {
+		panic(err)
+	}
+	// 设置键值
+	key := "key"
+	val := "value"
+	err := client.Set(key, val, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+	// 获取键值
+	val, err = client.Get(key).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s: %s\n", key, val)
+	// Output:
+	// key: value
+}
